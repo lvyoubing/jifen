0f295e46f6e829a5090aaacbd42c7b38456ab6e7
lvyoubing <244252204@qq.com>
2016-05-06 18:05:43 +0800

    5.6

---
+++ jifen/Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h
+//  jifen
+//
+//  Created by 吕欢达 on 16/5/6.
+//  Copyright © 2016年 吕欢达. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+
+#endif /* Bridging_Header_h */
+#import "sqlite3.h"
+#import <time.h>
---
+++ jifen/jifen-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  jifen-Bridging-Header.h
+//  jifen
+//
+//  Created by 吕欢达 on 16/5/6.
+//  Copyright © 2016年 吕欢达. All rights reserved.
+//
+
+#ifndef jifen_Bridging_Header_h
+#define jifen_Bridging_Header_h
+
+
+#endif /* jifen_Bridging_Header_h */
+#import "sqlite3.h"
+#import <time.h>
