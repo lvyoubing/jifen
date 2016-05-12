fdeea37b77bf3d7fac5b70e40ad1674888dba58b
lvyoubing <244252204@qq.com>
2016-05-12 11:31:47 +0800

    5.12

--- jifen/c.swift
+++ jifen/c.swift
@@ -10,14 +10,22 @@ import UIKit
 
 class c: UIViewController {
     var db:SQLiteDB!
-
-  
+    var a=0
+    var b=0
+    var c=0
+    var d=0
+    var e=0
+    var f=0
     @IBOutlet var txtUname: UITextField!
     @IBOutlet var txtMobile: UITextField!
     @IBOutlet var score1: UITextField!
     @IBOutlet var score2: UITextField!
-    var a=0
-    var b=0
+    @IBOutlet var fangui1: UITextField!
+    @IBOutlet var fangui2: UITextField!
+    @IBOutlet var weili1: UITextField!
+    @IBOutlet var weili2: UITextField!
+    @IBOutlet var huanren1: UITextField!
+    @IBOutlet var huanren2: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         db = SQLiteDB.sharedInstance()
@@ -26,6 +34,7 @@ class c: UIViewController {
         //如果有数据则加载
         initUser()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
     
     @IBAction func saveClicked(sender: AnyObject) {
@@ -44,26 +53,149 @@ class c: UIViewController {
         }
         saveUser()
     }
-
-    @IBAction func onesub(sender: UIButton) {
+    
+    @IBAction func two(sender: UIButton) {
         if(!score1.text!.isEmpty){
-            b=(score1.text!as NSString).integerValue
-            if b>0{
-                b=b-1
-                score1.text=("\(b)")
-            }
+        b=(score1.text!as NSString).integerValue
+        b=b+2
             score1.text=("\(b)")
-        }else
-        {
-            if b>0{
-                b=b-1
-                score1.text=("\(b)")
-            }
+        }
+        else{
+            b=b+2
             score1.text=("\(b)")
         }
         saveUser()
     }
-    func initUser() {
+    
+    @IBAction func three(sender: UIButton) {
+        if(!score1.text!.isEmpty){
+            b=(score1.text!as NSString).integerValue
+            b=b+3
+            score1.text=("\(b)")
+        }
+        else{
+            b=b+3
+            score1.text=("\(b)")
+        }
+        saveUser()
+    }
+    @IBAction func one2(sender: UIButton) {        if(!score2.text!.isEmpty){
+        a=(score2.text!as NSString).integerValue
+        a=a+1
+        score2.text=("\(a)")
+    }
+    else{
+        a=a+1
+        score2.text=("\(a)")
+        }
+        saveUser()
+        
+    }
+    
+    @IBAction func two2(sender: UIButton) {
+        if(!score2.text!.isEmpty){
+            a=(score2.text!as NSString).integerValue
+            a=a+2
+            score2.text=("\(a)")
+        }
+        else{
+            a=a+2
+            score2.text=("\(a)")
+        }
+        saveUser()
+        
+    }
+    @IBAction func three2(sender: UIButton) {
+        if(!score2.text!.isEmpty){
+            a=(score2.text!as NSString).integerValue
+            a=a+3
+            score2.text=("\(a)")
+        }
+        else{
+            a=a+3
+            score2.text=("\(a)")
+        }
+            saveUser()
+    }
+    
+    @IBAction func fangui1(sender: UIButton) {
+        if(!fangui1.text!.isEmpty){
+            a=(fangui1.text!as NSString).integerValue
+            a=a+1
+            fangui1.text=("\(a)")
+        }
+        else{
+            a=a+1
+            fangui1.text=("\(a)")
+        }
+        saveUser()
+
+    }
+    @IBAction func fangui2(sender: UIButton) {
+        if(!fangui2.text!.isEmpty){
+            b=(fangui2.text!as NSString).integerValue
+            b=b+1
+            fangui2.text=("\(b)")
+        }
+        else{
+            b=b+1
+            fangui2.text=("\(b)")
+        }
+        saveUser()
+
+    }
+    @IBAction func weili(sender: UIButton) {
+        if(!weili1.text!.isEmpty){
+            c=(weili1.text!as NSString).integerValue
+            c=c+1
+            weili1.text=("\(c)")
+        }
+        else{
+            c=c+1
+            weili1.text=("\(c)")
+        }
+        saveUser()
+    }
+    @IBAction func weili2(sender: UIButton) {
+        if(!weili2.text!.isEmpty){
+            d=(weili2.text!as NSString).integerValue
+            d=d+1
+            weili2.text=("\(d)")
+        }
+        else{
+            d=d+1
+            weili2.text=("\(d)")
+        }
+        saveUser()
+
+    }
+    @IBAction func huanren1(sender: UIButton) {
+        if(!huanren1.text!.isEmpty){
+            e=(huanren1.text!as NSString).integerValue
+            e=e+1
+            huanren1.text=("\(e)")
+        }
+        else{
+            e=e+1
+           huanren1.text=("\(e)")
+        }
+        saveUser()
+    }
+    @IBAction func huanren2(sender: UIButton) {
+        if(!huanren2.text!.isEmpty){
+            f=(huanren2.text!as NSString).integerValue
+            f=f+1
+            huanren2.text=("\(f)")
+        }
+        else{
+            f=f+1
+            huanren2.text=("\(f)")
+        }
+        saveUser()
+
+    }
+    
+      func initUser() {
         let data = db.query("select * from t_user")
         if data.count > 0 {
             //获取最后一行数据显示
